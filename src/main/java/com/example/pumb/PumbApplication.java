7a0706f9fc9e94bd0d9f5177eaf46b85180e3f8a
vovan4ok <bosik_ck@ukr.net>
2024-04-29 18:18:04 +0300

    Initial commit

---
+++ src/main/java/com/example/pumb/PumbApplication.java
@@ -0,0 +1,13 @@
+package com.example.pumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pumb/PumbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pumb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
